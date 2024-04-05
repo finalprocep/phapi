bedae0ce6961936101f17a84a2382d3a7a4c646f
finalprocep <166139660+finalprocep@users.noreply.github.com>
2024-04-05 23:38:42 +0530

    Update main.py added orthogonal

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import load_model
+from keras.initializers import Orthogonal
 
 app = FastAPI()
 

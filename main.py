b5cfc3a3e027491b1188e764c76236adaff89381
finalprocep <166139660+finalprocep@users.noreply.github.com>
2024-04-07 15:05:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,29 +29,29 @@ except FileNotFoundError:
 @app.post('/model_api')
 def predict():
 
-    scaler=MinMaxScaler(feature_range=(0,1))
-    timeinterval=24
-    prediction=12
+    # scaler=MinMaxScaler(feature_range=(0,1))
+    # timeinterval=24
+    # prediction=12
 
-    testapi='https://api.twelvedata.com/time_series?symbol=BTC/INR&interval=5min&outputsize=288&apikey=e76157c75c3a42649e168c5c206e88ca'
-    testdata=requests.get(testapi).json()
-    testdatafinal=pd.DataFrame(testdata['values'])
-    testinputs=testdatafinal['close'].values
-    testinputs=testinputs.reshape(-1,1)
-    modelinputs=scaler.fit_transform(testinputs)
+    # testapi='https://api.twelvedata.com/time_series?symbol=BTC/INR&interval=5min&outputsize=288&apikey=e76157c75c3a42649e168c5c206e88ca'
+    # testdata=requests.get(testapi).json()
+    # testdatafinal=pd.DataFrame(testdata['values'])
+    # testinputs=testdatafinal['close'].values
+    # testinputs=testinputs.reshape(-1,1)
+    # modelinputs=scaler.fit_transform(testinputs)
 
-    x_test=[]
-    for x in range(timeinterval,len(modelinputs)):
-        x_test.append(modelinputs[x-timeinterval:x,0])
+    # x_test=[]
+    # for x in range(timeinterval,len(modelinputs)):
+    #     x_test.append(modelinputs[x-timeinterval:x,0])
 
-    x_test=np.array(x_test)
-    x_test=np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
-    prediction_price=model.predict(x_test)
-    prediction_price=scaler.inverse_transform(prediction_price)
+    # x_test=np.array(x_test)
+    # x_test=np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+    # prediction_price=model.predict(x_test)
+    # prediction_price=scaler.inverse_transform(prediction_price)
 
-    lastdata=modelinputs[len(modelinputs)+1-timeinterval:len(modelinputs)+1,0]
-    lastdata=np.array(lastdata)
-    lastdata=np.reshape(lastdata,(1,lastdata.shape[0],1))
-    prediction=model.predict(lastdata)
-    prediction=scaler.inverse_transform(prediction)
-    return {"prediction":prediction[0][0]}
+    # lastdata=modelinputs[len(modelinputs)+1-timeinterval:len(modelinputs)+1,0]
+    # lastdata=np.array(lastdata)
+    # lastdata=np.reshape(lastdata,(1,lastdata.shape[0],1))
+    # prediction=model.predict(lastdata)
+    # prediction=scaler.inverse_transform(prediction)
+    return {"prediction":"api working successfully"}
